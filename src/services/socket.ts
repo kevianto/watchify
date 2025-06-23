18c450c72d11d3a2874cb1a428b98d8280ae4d54
kevianto <kevinshahale746@gmail.com>
2025-06-23 12:38:13 +0300

    socket

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000';
+const SOCKET_URL = 'https://watchify-backend-1.onrender.com';
 
 class SocketService {
   private socket: Socket | null = null;
